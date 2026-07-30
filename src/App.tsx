04bf82fa91c034dda465181d05aa056a9bc986c8
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-07-30 20:30:16 +0300

    added toast provider in app.tsx so its mounted in all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css'
 import AppRoutes from './routes/AppRoutes'
+import { ToastProvider } from './services/toast'
 
 function App() {
-  return <AppRoutes />
+  return <>
+    <ToastProvider />
+    <AppRoutes />
+  </>
 }
 
 export default App
