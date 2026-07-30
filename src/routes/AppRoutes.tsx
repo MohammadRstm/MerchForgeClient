fa46e05d971c6021b98c6c0526f50d771c94322d
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-07-30 13:50:35 +0300

    completed AppRoutes configuration

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,9 +1,21 @@
-import { Routes } from "react-router"
+import { Route, Routes } from "react-router"
+import PagesWithHeaderLayout from "../layout/PagesWithHeaders/PagesWithHeaders";
+import AuthenticatedRoutes from "./AuthenticatedRoutes";
+import ErrorBoundaryRoutes from "./ErrorBoundaryRoutes";
 
 const AppRoutes = () =>{
 
     return(
         <Routes>
+
+            <Route element={<PagesWithHeaderLayout />}>
+
+                <Route element={<ErrorBoundaryRoutes />}>
+                </Route>
+
+                <Route element={<AuthenticatedRoutes />}>
+                </Route>
+            </Route>
 
         </Routes>
     );
