34ae9d69a2b9fafd55f599a7d61405f54ce709ec
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-07-30 13:33:19 +0300

    created error fallback page in case of redering errors

---
+++ src/features/ErrorFallBack/ErrorFallBack.tsx
@@ -0,0 +1,36 @@
+import type { FallbackProps } from "react-error-boundary";
+import "./ErrorFallback.css";
+
+const ErrorFallback = ({ resetErrorBoundary }: FallbackProps) => {
+    return (
+        <div className="error-page">
+
+            <div className="error-card">
+
+                <div className="error-icon">
+                    !
+                </div>
+
+                <h1 className="error-title">
+                    Oops!
+                </h1>
+
+                <p className="error-text">
+                    Something went wrong while loading this page.
+                    Please try again.
+                </p>
+
+                <button
+                    className="error-btn"
+                    onClick={resetErrorBoundary}
+                >
+                    Try Again
+                </button>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default ErrorFallback;
