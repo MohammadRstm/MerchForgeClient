565ca2967eef1bc2fd0221c138a6701d2d97a7df
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-07-31 13:03:57 +0300

    create api route constants to eliminate magical strings

---
+++ src/services/api/apiRoutes.ts
@@ -0,0 +1,10 @@
+export const apiRoutes = {
+    
+    /** AUTH */
+    AUTH_LOGIN : "/auth/login",
+    AUTH_SIGNUP : "/auth/signup",
+
+    /** USER */
+    USER_PROFILE : "/profile",
+    
+}
--- src/services/api/auth.api.ts
+++ src/services/api/auth.api.ts
@@ -1,10 +1,11 @@
 import type { LoginFormDataType, LoginResponse } from "../../features/Auth/Login/types";
 import { loginResponseSchema } from "../../features/Auth/Login/validation";
 import { unAuthenticatedApi } from "./api";
+import { apiRoutes } from "./apiRoutes";
 
 export const loginService = async (loginFormData : LoginFormDataType) : Promise<LoginResponse> =>{
     const { data } = await unAuthenticatedApi.post<LoginResponse>(
-        "/login" ,
+        apiRoutes.AUTH_LOGIN ,
         loginFormData
     );
 
