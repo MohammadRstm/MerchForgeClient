cfd88d8d47fa823b83025a5ef51cfae9127268d6
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-07-31 12:41:09 +0300

    gave login the header component

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -11,9 +11,9 @@ const AppRoutes = () =>{
     return(
         <Routes>
             <Route element={<ErrorBoundaryRoutes />}>
-                <Route path={routes.LOGIN} element={<Login />} />
 
                 <Route element={<PagesWithHeaderLayout />}>
+                    <Route path={routes.LOGIN} element={<Login />} />
                     <Route path={routes.HOME} element={<Home />} />
                     <Route element={<AuthenticatedRoutes />}>
                         {/** EX USER PROFILE */}
