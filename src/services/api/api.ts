d73ee81d6a06731c060563a4b7e5ae12b2aa4661
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-18 03:52:43 +0300

    feat:hooked api url to axios interceptors

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,12 +1,20 @@
 import axios from "axios";
 import { env } from "../../config/env";
 
+const token = localStorage.getItem("token");
+
+const apiUrl = env.apiUrl + "api/";
+
 export const authenticatedApi = axios.create({
-    baseURL: env.apiUrl,
+    baseURL: apiUrl,
+    headers:{
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`
+    }
 });
 
 export const unAuthenticatedApi = axios.create({
-    baseURL: env.apiUrl,
+    baseURL: apiUrl,
     headers: {
         Accept: "application/json",
     },
