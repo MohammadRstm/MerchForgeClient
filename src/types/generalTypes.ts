f86ff8b4195b19c75bbdfffbcf12e6636496ddf7
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-18 03:46:47 +0300

    feat:Created api error class compatible with backend

--- src/types/generalTypes.ts
+++ src/types/generalTypes.ts
@@ -8,3 +8,18 @@ export interface User{
 
 export type FormErrors<T> = Partial<Record<keyof T, string>>;
 
+export type ErrorType =
+    | "Validation"
+    | "Authentication"
+    | "Authorization"
+    | "NotFound"
+    | "Conflict"
+    | "Unexpected";
+
+export interface ApiErrorResponse {
+    type: ErrorType;
+    code: string;
+    message: string;
+    traceId: string;
+    errors?: Record<string, string[]>;
+}
