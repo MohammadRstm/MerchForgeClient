b6ae9100684d9b18d20902168328400423b1483a
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-19 08:27:24 +0300

    feat: add business dashboard API routes

--- src/services/api/apiRoutes.ts
+++ src/services/api/apiRoutes.ts
@@ -13,4 +13,10 @@ export const apiRoutes = {
     DASHBOARD_BUSINESSES : "/Dashboard/businesses",
     DASHBOARD_REVOKE_USER_SESSIONS : (userId : string) => `/Dashboard/users/${userId}/revoke-sessions`,
 
+    /** BUSINESS DASHBOARD */
+    BUSINESS_DASHBOARD_STATS : (businessId : string) => `/businesses/${businessId}/dashboard/stats`,
+    BUSINESS_DASHBOARD_PRODUCTS : (businessId : string) => `/businesses/${businessId}/dashboard/products`,
+    BUSINESS_DASHBOARD_MEMBERS : (businessId : string) => `/businesses/${businessId}/dashboard/members`,
+    BUSINESS_DASHBOARD_SUBSCRIPTION : (businessId : string) => `/businesses/${businessId}/dashboard/subscription`,
+
 }
